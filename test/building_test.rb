16457cfc1d604606120f6f1fdbf6d8151f8b7d88
Austin Aspaas <araspaas@live.com>
2020-07-14 09:39:18 -0600

    Add the add unit test

--- test/building_test.rb
+++ test/building_test.rb
@@ -9,6 +9,8 @@ class BuildingTest < Minitest::Test
 
   def setup
     @building = Building.new
+    @unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    @unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
   end
 
   def test_it_exists
@@ -17,5 +19,12 @@ class BuildingTest < Minitest::Test
 
   def test_units_starts_empty
     assert_equal [], @building.units
+  end
+
+  def test_it_can_add_units
+    @building.add_unit(@unit1)
+    @building.add_unit(@unit2)
+
+    assert_equal [@unit1, @unit2], @building.units
   end
 end
