3e3ceec34ede579a94c6c1ef4496a5e7b740640f
Austin Aspaas <araspaas@live.com>
2020-07-14 09:23:06 -0600

    Update initialize method and add attr reader for attributes. pass test

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,6 +1,15 @@
 class Apartment
+  attr_reader :number,
+              :monthly_rent,
+              :bathrooms,
+              :bedrooms,
+              :renter
 
-  def initialize(argument)
-    @argument = argument
+  def initialize(apartment_params)
+    @number = apartment_params[:number]
+    @monthly_rent = apartment_params[:monthly_rent]
+    @bathrooms = apartment_params[:bathrooms]
+    @bedrooms = apartment_params[:bedrooms]
+    @renter = nil
   end
 end
