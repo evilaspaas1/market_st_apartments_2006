b82c387c00d51063828724b7d9d1d17d6258393e
Austin Aspaas <araspaas@live.com>
2020-07-14 09:06:56 -0600

    Add test it exists test

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -6,6 +6,10 @@ require 'pry'
 class RenterTest < Minitest::Test
 
   def setup
-    
+    @renter1 = Renter.new("Jessie")
+  end
+
+  def test_it_exists
+    assert_instance_of Renter, @renter1
   end
 end
