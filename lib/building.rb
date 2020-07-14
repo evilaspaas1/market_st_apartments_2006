bb471172c3deb640d7e0e6666cd85df7f8162494
Austin Aspaas <araspaas@live.com>
2020-07-14 10:12:13 -0600

    Add average rent method and pass test

--- lib/building.rb
+++ lib/building.rb
@@ -14,4 +14,12 @@ class Building
   def add_renter(renter)
     @renters << renter.name
   end
+
+  def average_rent
+    average_rent = []
+    @units.each do |unit|
+      average_rent << unit.monthly_rent
+    end
+    (average_rent.sum).to_f / 2
+  end
 end
